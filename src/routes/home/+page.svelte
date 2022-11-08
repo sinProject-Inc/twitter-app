6f9edf1526a5ae5d75d725d15b6375b5a9ea67cc
sinYa Iwasaki <iwasaki@sinproject.net>
2022-11-08 15:14:15 +0900

    Show timeline #11

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import type { PageData } from './$types'
+
+	export let data: PageData
+
+	console.log(data.tweets)
+
+	const tweets = data.tweets.data || []
+</script>
+
+<svelte:head>
+	<title>Home / Twitter App</title>
+</svelte:head>
+
+<form action="/sign_out" method="POST">
+	<button type="submit">Log out</button>
+</form>
+
+{$page.data.user.twitter_id}
+
+{#each tweets as tweet}
+	<div>
+		{tweet.text}
+	</div>
+{/each}
+
+{data.tweets}
+
+
