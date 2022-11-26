95b6b52f77b2454dede70f50562dc722d8d10175
sinYa Iwasaki <iwasaki@sinproject.net>
2022-11-27 02:01:11 +0900

    Show retweets #26

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -10,18 +10,18 @@ export const load: PageServerLoad = async ({ locals }) => {
 	try {
 		const tweets = await client.tweets.usersIdTimeline(locals.user.twitter_id, {
 			// max_results: 30,
-			expansions: ['author_id', 'referenced_tweets.id'],
+			expansions: ['author_id', 'referenced_tweets.id', 'referenced_tweets.id.author_id'],
 			'tweet.fields': ['created_at'],
 			'user.fields': ['profile_image_url'],
 			// 'tweet.fields': ['author_id'],
 			// 'tweet.fields': ['created_at', 'entities', 'public_metrics'],
 		})
 
-		if (tweets.data) {
-			for (const tweet of tweets.data) {
-				console.log('referenced', tweet.referenced_tweets)
-			}
-		}
+		// if (tweets.data) {
+		// 	for (const tweet of tweets.data) {
+		// 		console.log('referenced', tweet.referenced_tweets)
+		// 	}
+		// }
 
 		console.log(tweets)
 		// console.log(tweets.includes?.users)
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Retweet from '$lib/icons/retweet.svelte'
+	import type { components } from 'twitter-api-sdk/dist/types'
 	import type { PageData } from './$types'
 
 	export let data: PageData
@@ -13,42 +15,79 @@
 		map.set(user.id, user)
 		return map
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	}, new Map<string, any>())
+	}, new Map<string, components['schemas']['User']>())
 
 	const referenced_tweets_map = referenced_tweets.reduce((map, tweet) => {
 		map.set(tweet.id, tweet)
 		return map
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	}, new Map<string, any>())
+	}, new Map<string, components['schemas']['Tweet']>())
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	function get_retweeted(tweet: any): any {
-		if (tweet.referenced_tweets) {
-			const referenced_tweet = tweet.referenced_tweets[0]
+	// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+	// function get_retweeted(tweet: any): any {
+	// 	if (tweet.referenced_tweets) {
+	// 		const referenced_tweet = tweet.referenced_tweets[0]
 
-			if (referenced_tweet?.type === 'retweeted') {
-				const retweeted_tweet = referenced_tweets_map.get(referenced_tweet.id)
-				const retweeted_user = user_map.get(retweeted_tweet?.author_id)
+	// 		if (referenced_tweet?.type === 'retweeted') {
+	// 			const retweeted_tweet = referenced_tweets_map.get(referenced_tweet.id)
+	// 			const retweeted_user = user_map.get(retweeted_tweet?.author_id)
 
-				return { tweet: retweeted_tweet, user: retweeted_user }
-			}
-		}
-	}
+	// 			return { tweet: retweeted_tweet, user: retweeted_user }
+	// 		}
+	// 	}
+	// }
 
-	const timeline = tweets.map((tweet) => {
-		const user = user_map.get(tweet.author_id ?? '')
-		const retweeted = get_retweeted(tweet)
+	// const timeline = tweets.map((tweet) => {
+	// 	const user = user_map.get(tweet.author_id ?? '')
+	// 	const retweeted = get_retweeted(tweet)
 
-		return { tweet, user, retweeted }
-	})
+	// 	return { tweet, user, retweeted }
+	// })
 
-	console.log(timeline)
+	// console.log(timeline)
 
 	function toLocalDate(date?: string): string {
 		if (!date) return ''
 
 		return new Date(date).toLocaleString('ja-JP')
 	}
+
+	function isRetweet(tweet: components['schemas']['Tweet']): boolean {
+		return (
+			tweet.referenced_tweets?.some((referenced_tweet) => referenced_tweet.type === 'retweeted') ??
+			false
+		)
+	}
+
+	function getTargetTweet(tweet: components['schemas']['Tweet']): components['schemas']['Tweet'] {
+		if (tweet.referenced_tweets) {
+			const referenced_tweet = tweet.referenced_tweets[0]
+
+			if (referenced_tweet?.type === 'retweeted') {
+				const retweeted_tweet = referenced_tweets_map.get(referenced_tweet.id)
+				return retweeted_tweet ?? tweet
+			}
+		}
+
+		return tweet
+	}
+
+	function getUser(
+		tweet: components['schemas']['Tweet'],
+		isOriginal = false
+	): components['schemas']['User'] | undefined {
+		const target_tweet = isOriginal ? tweet : getTargetTweet(tweet)
+
+		return user_map.get(target_tweet.author_id ?? '')
+	}
+
+	function getText(tweet: components['schemas']['Tweet']): string {
+		const target_tweet = getTargetTweet(tweet)
+
+		return target_tweet.text ?? ''
+	}
+
+	// console.log(tweets)
 </script>
 
 <svelte:head>
@@ -63,17 +102,24 @@
 
 <!-- {$page.data.user.twitter_id} -->
 
-{#each timeline as timeline_item}
+{#each tweets as tweet}
 	<div class="element">
-		<img class="avatar" src={timeline_item.user?.profile_image_url ?? ''} alt="profile" />
-		<div class="text_column">
-			<div class="username_row">
-				<div class="name overflow_ellipsis">{timeline_item.user?.name}</div>
-				<div class="username overflow_ellipsis">@{timeline_item.user?.username}</div>
-				<div class="time">{toLocalDate(timeline_item.tweet.created_at)}</div>
+		{#if isRetweet(tweet)}
+			<div class="retweet_row">
+				<div class="avatar_above"><div class="retweet"><Retweet /></div></div>
+				{getUser(tweet, true)?.name}さんがリツイートしました
 			</div>
-			<!-- {timeline_item.tweet.text} -->
-			{timeline_item.tweet.referenced_tweets}
+		{/if}
+		<div class="tweet">
+			<img class="avatar" src={getUser(tweet)?.profile_image_url} alt="avatar" />
+			<div class="text_column">
+				<div class="username_row">
+					<div class="name overflow_ellipsis">{getUser(tweet)?.name}</div>
+					<div class="username overflow_ellipsis">@{getUser(tweet)?.username}</div>
+					<div class="time">{toLocalDate(tweet.created_at)}</div>
+				</div>
+				{getText(tweet)}
+			</div>
 		</div>
 	</div>
 {/each}
@@ -84,8 +130,33 @@
 	}
 
 	.element {
-		padding: 8px;
+		padding: 10px;
 		border-bottom: 1px solid #ccc;
+		display: flex;
+		flex-direction: column;
+		gap: 6px;
+	}
+
+	.retweet_row {
+		display: flex;
+		flex-direction: row;
+		gap: 8px;
+		align-items: center;
+		height: 22px;
+	}
+
+	.avatar_above {
+		width: 48px;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+	}
+
+	.retweet {
+		width: 16px;
+	}
+
+	.tweet {
 		display: flex;
 		flex-direction: row;
 		gap: 8px;
@@ -100,7 +171,7 @@
 	.text_column {
 		display: flex;
 		flex-direction: column;
-		gap: 4px;
+		gap: 2px;
 		min-width: 0;
 	}
 
