80159cbf42f1eddb37f95c9488cfb54ab8c88741
Ryan <ryan@websculptors.net>
2022-12-03 17:01:53 +0900

    取得できていない画像を、別APIから取得する #69

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -28,6 +28,32 @@ export const load: PageServerLoad = async ({ locals }) => {
 		// 		console.log('referenced', tweet.referenced_tweets)
 		// 	}
 		// }
+		const retweetIds = []
+		if (tweets.data) {
+			for (const tweet of tweets.data) {
+				if (tweet.referenced_tweets) {
+					for (const refTweet of tweet.referenced_tweets) {
+						if (refTweet.type == "retweeted") {
+							retweetIds.push(refTweet.id)
+						}
+					}
+				}
+			}
+		}
+
+		const retweets = await twitterClient.tweets.findTweetsById({
+			ids: retweetIds,
+			expansions: [
+				'attachments.media_keys',
+			],
+			'tweet.fields': ['created_at', 'public_metrics', 'entities'],
+			'user.fields': ['profile_image_url'],
+			'media.fields': ['preview_image_url', 'url'],
+		})
+
+		if (tweets.includes?.media && retweets.includes?.media) {
+			tweets.includes.media.push(...retweets.includes.media)
+		}
 
 		console.log('tweets', tweets)
 		console.log('media', tweets.includes?.media)
