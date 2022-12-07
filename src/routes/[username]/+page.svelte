6b3e0bd9cb75525f600374c2fd534f4e165dbb7c
Ryan <ryan@websculptors.net>
2022-12-07 20:29:32 +0900

    feat: Add basic layout

---
+++ src/routes/[username]/+page.svelte
@@ -0,0 +1,339 @@
+<script lang="ts">
+  	import '../app.css'
+</script>
+
+<div class="gap_border root_container">
+	<div class="center_container">
+      <div class="header header2">
+        <div class="header_pic_container">
+          <svg viewBox="0 0 24 24" aria-hidden="true"
+            ><g><path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z" /></g></svg>
+        </div>
+        <div class="header_text_container">
+          <div class="header_name">sinYa Iwasaki いわしん</div>
+          <div class="header_tweet_count">8.9万 件のツイート</div>
+        </div>
+      </div>
+      <div class="header_image_container" />
+      <div class="profile_container">
+        <div class="pic_container" />
+        <div class="buttons_container">
+          <div class="more_button">
+            <svg viewBox="0 0 24 24" aria-hidden="true"
+              ><g
+                ><path
+                  d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z" /></g
+              ></svg>
+          </div>
+          <div class="follow_button ">フォロー</div>
+        </div>
+        <div class="name_container">
+          <div class="name">sinYa Iwasaki いわしん</div>
+          <div class="username">@iam_o_sin</div>
+        </div>
+        <div class="bio">
+          株式会社 <block class="hashtag">#sinProject</block> CEO/CTO
+          <br />
+          世界を幸せにするアプリを作る
+          <br />
+          <br />
+          Twitterアプリ1位 <block class="hashtag">#Tweecha2</block> 企画制作
+          <br /> システム設計開発、ITコンサルタント、<block class="hashtag">#sinPro式学習</block>
+          <br />
+          <block class="hashtag">#Svelte</block> <block class="hashtag">#TypeScript</block>
+          <block class="hashtag">#Flutter</block> <block class="hashtag">#Dart</block>
+          <block class="hashtag">#Java</block> <br />
+          <block class="hashtag">#kimurakan</block> ピアノ弾き語り
+        </div>
+        <div class="description">
+          <div class="description_row_container">
+            <div class="description_item_container">
+              <div class="description_item_icon">
+                <svg viewBox="0 0 24 24" aria-hidden="true">
+                  <g>
+                    <path
+                      d="M12 7c-1.93 0-3.5 1.57-3.5 3.5S10.07 14 12 14s3.5-1.57 3.5-3.5S13.93 7 12 7zm0 5c-.827 0-1.5-.673-1.5-1.5S11.173 9 12 9s1.5.673 1.5 1.5S12.827 12 12 12zm0-10c-4.687 0-8.5 3.813-8.5 8.5 0 5.967 7.621 11.116 7.945 11.332l.555.37.555-.37c.324-.216 7.945-5.365 7.945-11.332C20.5 5.813 16.687 2 12 2zm0 17.77c-1.665-1.241-6.5-5.196-6.5-9.27C5.5 6.916 8.416 4 12 4s6.5 2.916 6.5 6.5c0 4.073-4.835 8.028-6.5 9.27z" /></g>
+                </svg>
+              </div>
+              <div>大阪市西区新町1-22-22</div>
+            </div>
+            <div class="description_item_container">
+              <div class="description_item_icon">
+                <svg viewBox="0 0 24 24" aria-hidden="true"
+                  ><g
+                    ><path
+                      d="M18.36 5.64c-1.95-1.96-5.11-1.96-7.07 0L9.88 7.05 8.46 5.64l1.42-1.42c2.73-2.73 7.16-2.73 9.9 0 2.73 2.74 2.73 7.17 0 9.9l-1.42 1.42-1.41-1.42 1.41-1.41c1.96-1.96 1.96-5.12 0-7.07zm-2.12 3.53l-7.07 7.07-1.41-1.41 7.07-7.07 1.41 1.41zm-12.02.71l1.42-1.42 1.41 1.42-1.41 1.41c-1.96 1.96-1.96 5.12 0 7.07 1.95 1.96 5.11 1.96 7.07 0l1.41-1.41 1.42 1.41-1.42 1.42c-2.73 2.73-7.16 2.73-9.9 0-2.73-2.74-2.73-7.17 0-9.9z" /></g
+                  ></svg>
+              </div>
+              <div>sinproject.net</div>
+            </div>
+          </div>
+          <div class="description_item_container">
+            <div class="description_item_icon">
+              <svg viewBox="0 0 24 24" aria-hidden="true"
+                ><g
+                  ><path
+                    d="M7 4V3h2v1h6V3h2v1h1.5C19.89 4 21 5.12 21 6.5v12c0 1.38-1.11 2.5-2.5 2.5h-13C4.12 21 3 19.88 3 18.5v-12C3 5.12 4.12 4 5.5 4H7zm0 2H5.5c-.27 0-.5.22-.5.5v12c0 .28.23.5.5.5h13c.28 0 .5-.22.5-.5v-12c0-.28-.22-.5-.5-.5H17v1h-2V6H9v1H7V6zm0 6h2v-2H7v2zm0 4h2v-2H7v2zm4-4h2v-2h-2v2zm0 4h2v-2h-2v2zm4-4h2v-2h-2v2z" /></g
+                ></svg>
+            </div>
+            <div>2009年3月からTwitterを利用しています</div>
+          </div>
+        </div>
+        <div class="follow_details_container">
+          <div>
+            <block class="follow_details_header">4.2万</block>フォロー中
+          </div>
+          <div>
+            <block class="follow_details_header">4.3万</block>フォロワー
+          </div>
+        </div>
+        <div class="follow_details_mutuals">フォローしている人にフォロワーはいません</div>
+        <div class="tab_container">
+          <div class="selected_tab tab">
+            <div />
+            <div>ツイート</div>
+            <div class="tab_underline selected_tab_underline" />
+          </div>
+          <div class="tab">
+            <div />
+            <div>ツイートと返信</div>
+            <div class="tab_underline" />
+          </div>
+          <div class="tab">
+            <div />
+            <div>メディア</div>
+            <div class="tab_underline" />
+          </div>
+          <div class="tab">
+            <div />
+            <div>いいね</div>
+            <div class="tab_underline" />
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+<style>
+	.header {
+		-webkit-position: sticky;
+		position: sticky;
+		z-index: 3;
+		top: 0;
+		height: 53px;
+		backdrop-filter: blur(12px);
+		background-color: rgba(255, 255, 255, 0.85);
+		padding-left: 16px;
+		padding-right: 16px;
+		font-size: 22px;
+		font-weight: 700;
+	}
+
+	.top_container {
+		display: flex;
+		flex-direction: row;
+		height: 2000px;
+	}
+
+	.header_container {
+		min-width: 0;
+		width: 600px;
+		margin-left: auto;
+		margin-right: auto;
+		border-left-width: 1px;
+		border-right-width: 1px;
+	}
+
+	.header2 {
+		display: flex;
+		padding-left: 0.5rem;
+		padding-right: 0.5rem;
+		align-items: center;
+		gap: 1.5rem;
+		height: 53px;
+	}
+
+	.header_pic_container {
+		margin: 0.5rem;
+		width: 1.25rem;
+		height: 1.25rem;
+		border-radius: 9999px;
+	}
+
+	.header_text_container {
+		padding-top: 0.25rem;
+		line-height: 1rem;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.header_name {
+		font-weight: 700;
+		margin-bottom: 6px;
+		font-size: 20px;
+		color: rgb(15, 20, 25);
+	}
+
+	.header_tweet_count {
+		font-size: 13.5px;
+		color: rgb(83, 100, 113);
+	}
+
+	.header_image_container {
+		width: 100%;
+		height: 200px;
+	}
+
+	.profile_container {
+		border-bottom-width: 1px;
+		padding-left: 16px;
+		padding-right: 16px;
+	}
+
+	.pic_container {
+		position: absolute;
+		border-radius: 9999px;
+		border-width: 4px;
+		border-color: #ffffff;
+		height: 140px;
+		width: 140px;
+		--tw-translate-y: -70px;
+		transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x))
+			skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+	}
+
+	.buttons_container {
+		display: flex;
+		justify-content: flex-end;
+    align-items: center;
+		margin-bottom: 23px;
+		padding-bottom: 12px;
+		gap: 8px;
+	}
+
+	.more_button {
+		border-radius: 9999px;
+		border-width: 1px;
+		height: 20px;
+		width: 20px;
+		padding: 7px;
+    border: solid #CFD9DD;
+	}
+
+	.follow_button {
+		color: #ffffff;
+		font-weight: 700;
+		background: rgb(15, 20, 25);
+		font-size: 16px;
+		border-radius: 26px;
+		padding-left: 17.5px;
+		padding-right: 17.5px;
+		padding-top: 8px;
+		padding-bottom: 8px;
+	}
+
+	.name_container {
+		margin-bottom: 16px;
+		line-height: 16px;
+	}
+
+	.name {
+		font-size: 1.25rem;
+		line-height: 1.75rem;
+		font-weight: 800;
+		color: rgb(15, 20, 26);
+	}
+
+	.username {
+		color: 15px;
+		color: rgb(83, 100, 113);
+	}
+
+	.bio {
+		padding-bottom: 0.75rem;
+		line-height: 1.25rem;
+		color: 16px;
+	}
+
+	.hashtag {
+		color: rgb(96 165 250);
+	}
+
+	.description {
+		margin-bottom: 11px;
+		color: rgb(83, 100, 113);
+	}
+
+	.description_row_container {
+		display: flex;
+		gap: 12px;
+	}
+
+	.description_item_container {
+		display: flex;
+		align-items: center;
+		gap: 4px;
+	}
+
+	.description_item_icon {
+		height: 19px;
+		width: 19px;
+		fill: rgb(83, 100, 113);
+	}
+
+	.follow_details_container {
+		display: flex;
+		margin-bottom: 0.5rem;
+		gap: 1.25rem;
+		color: 15px;
+		color: rgb(83, 100, 113);
+	}
+
+	.follow_details_header {
+		margin-right: 0.25rem;
+		color: #000000;
+		font-weight: 700;
+	}
+
+	.follow_details_mutuals {
+		margin-bottom: 1.25rem;
+		color: 14px;
+		color: rgb(83, 100, 113);
+	}
+
+	.tab_container {
+		display: flex;
+		text-align: center;
+		align-items: center;
+		height: 3.5rem;
+		color: rgb(83, 100, 113);
+	}
+
+	.tab {
+		display: flex;
+		padding-top: 0;
+		padding-bottom: 0;
+		font-size: 1.125rem;
+		line-height: 1.75rem;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		height: 100%;
+		flex-grow: 1;
+	}
+
+	.selected_tab {
+		color: black;
+		font-weight: 700;
+	}
+
+	.selected_tab_underline {
+		background-color: rgb(96 165 250);
+	}
+
+	.tab_underline {
+		width: 75%;
+		height: 0.25rem;
+		border-radius: 0.375rem;
+	}
+</style>
