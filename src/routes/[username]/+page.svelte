3827f8a1f32c6633b8d3df6bdc4ea75f4ee31112
Ryan <ryan@websculptors.net>
2022-12-13 23:13:07 +0900

    feat(profile): Add icons

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -4,6 +4,11 @@
 	import type { components } from 'twitter-api-sdk/dist/types'
 	import Loading from '$lib/icons/loading.svelte'
 	import '../app.css'
+	import LeftArrow from '$lib/icons/left_arrow.svelte'
+	import ThreeDot from '$lib/icons/three_dot.svelte'
+	import LocationPin from '$lib/icons/location_pin.svelte'
+	import Link from '$lib/icons/link.svelte'
+	import Calendar from '$lib/icons/calendar.svelte'
 
 	let user_data: components['schemas']['User']
 	let is_loading = false
@@ -38,13 +43,7 @@
 		<div class="center_container">
 			<div class="header header_items_container">
 				<div class="header_pic_container">
-					<svg viewBox="0 0 24 24" aria-hidden="true"
-						><g
-							><path
-								d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"
-							/></g
-						></svg
-					>
+					<LeftArrow />
 				</div>
 				<div class="header_text_container">
 					<div class="header_name">{user_data.name}</div>
@@ -62,13 +61,7 @@
 				{/if}
 				<div class="buttons_container">
 					<div class="more_button">
-						<svg viewBox="0 0 24 24" aria-hidden="true"
-							><g
-								><path
-									d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
-								/></g
-							></svg
-						>
+						<ThreeDot />
 					</div>
 					<div class="follow_button ">フォロー</div>
 				</div>
@@ -88,13 +81,7 @@
 						{#if user_data.location}
 							<div class="description_item_container">
 								<div class="description_item_icon">
-									<svg viewBox="0 0 24 24" aria-hidden="true">
-										<g>
-											<path
-												d="M12 7c-1.93 0-3.5 1.57-3.5 3.5S10.07 14 12 14s3.5-1.57 3.5-3.5S13.93 7 12 7zm0 5c-.827 0-1.5-.673-1.5-1.5S11.173 9 12 9s1.5.673 1.5 1.5S12.827 12 12 12zm0-10c-4.687 0-8.5 3.813-8.5 8.5 0 5.967 7.621 11.116 7.945 11.332l.555.37.555-.37c.324-.216 7.945-5.365 7.945-11.332C20.5 5.813 16.687 2 12 2zm0 17.77c-1.665-1.241-6.5-5.196-6.5-9.27C5.5 6.916 8.416 4 12 4s6.5 2.916 6.5 6.5c0 4.073-4.835 8.028-6.5 9.27z"
-											/></g
-										>
-									</svg>
+									<LocationPin />
 								</div>
 								<div>{user_data.location}</div>
 							</div>
@@ -102,13 +89,7 @@
 						{#if user_data.entities?.url?.urls}
 							<div class="description_item_container">
 								<div class="description_item_icon">
-									<svg viewBox="0 0 24 24" aria-hidden="true"
-										><g
-											><path
-												d="M18.36 5.64c-1.95-1.96-5.11-1.96-7.07 0L9.88 7.05 8.46 5.64l1.42-1.42c2.73-2.73 7.16-2.73 9.9 0 2.73 2.74 2.73 7.17 0 9.9l-1.42 1.42-1.41-1.42 1.41-1.41c1.96-1.96 1.96-5.12 0-7.07zm-2.12 3.53l-7.07 7.07-1.41-1.41 7.07-7.07 1.41 1.41zm-12.02.71l1.42-1.42 1.41 1.42-1.41 1.41c-1.96 1.96-1.96 5.12 0 7.07 1.95 1.96 5.11 1.96 7.07 0l1.41-1.41 1.42 1.41-1.42 1.42c-2.73 2.73-7.16 2.73-9.9 0-2.73-2.74-2.73-7.17 0-9.9z"
-											/></g
-										></svg
-									>
+									<Link />
 								</div>
 								<div>
 									<a target="_blank" rel="noreferrer" href={user_data.entities?.url?.urls[0]?.url}
@@ -120,13 +101,7 @@
 					</div>
 					<div class="description_item_container">
 						<div class="description_item_icon">
-							<svg viewBox="0 0 24 24" aria-hidden="true"
-								><g
-									><path
-										d="M7 4V3h2v1h6V3h2v1h1.5C19.89 4 21 5.12 21 6.5v12c0 1.38-1.11 2.5-2.5 2.5h-13C4.12 21 3 19.88 3 18.5v-12C3 5.12 4.12 4 5.5 4H7zm0 2H5.5c-.27 0-.5.22-.5.5v12c0 .28.23.5.5.5h13c.28 0 .5-.22.5-.5v-12c0-.28-.22-.5-.5-.5H17v1h-2V6H9v1H7V6zm0 6h2v-2H7v2zm0 4h2v-2H7v2zm4-4h2v-2h-2v2zm0 4h2v-2h-2v2zm4-4h2v-2h-2v2z"
-									/></g
-								></svg
-							>
+							<Calendar />
 						</div>
 						<div>2009年3月からTwitterを利用しています</div>
 					</div>
